cc965867fd034e4c09b43eb5ca5187a52e6870f1
IsuruX98 <isurusanka98@gmail.com>
2024-02-15 16:48:40 +0530

    remove unwanted file

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { FloatingNavDemo } from "@/components/ui/FloatingNavDemo";
 import Hero from "../components/Hero";
 import React from "react";
 import Education from "@/components/Education";
-import Projects from "@/components/Projects";
 import { CardHoverEffectDemo } from "@/components/ui/CardHoverEffectDemo";
 import Footer from "@/components/Footer";
 import Skills from "@/components/Skills";
