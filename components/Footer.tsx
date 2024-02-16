2ee34e2c025ab3dc5e42d40eda0d49ed92452560
IsuruX98 <isurusanka98@gmail.com>
2024-02-16 11:08:22 +0530

    footer links added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaFacebook, FaLinkedin, FaInstagram, FaGithub } from "react-icons/fa";
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -7,10 +8,21 @@ const Footer = () => {
       <div className="flex md:justify-between flex-col items-center gap-4 justify-center xl:px-36 lg:px-20 px-12 py-20">
         <div>Copyright © 2024 - All right reserved</div>
         <div className="flex gap-4">
-          <FaFacebook className="text-lg" />
-          <FaLinkedin className="text-lg" />
-          <FaInstagram className="text-lg" />
-          <FaGithub className="text-lg" />
+          <Link href="https://www.facebook.com/isuru.madusanka.1806/" passHref>
+            <FaFacebook className="text-lg" />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/isuru-madusanka-1b9210218/"
+            passHref
+          >
+            <FaLinkedin className="text-lg" />
+          </Link>
+          <Link href="https://www.instagram.com/izu._x/" passHref>
+            <FaInstagram className="text-lg" />
+          </Link>
+          <Link href="https://github.com/IsuruX98" passHref>
+            <FaGithub className="text-lg" />
+          </Link>
         </div>
       </div>
     </footer>
